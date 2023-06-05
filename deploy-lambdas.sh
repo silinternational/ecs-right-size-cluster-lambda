85ba1382b85ba19567c7e8c9af689009ece8f9fd
briskt <3172830+briskt@users.noreply.github.com>
2023-06-05 09:59:43 +0800

    only resize the cluster for the stage

--- deploy-lambdas.sh
+++ deploy-lambdas.sh
@@ -9,9 +9,12 @@ set -x
 # build binary
 go build -ldflags="-s -w" -o ecs-right-size-cluster
 
+export ClusterNamesCSV=ClusterNamesCSV_dev
+
 STAGE="dev"
 if [[ "${CI_BRANCH}" == "main" ]]; then
     STAGE="prod"
+    export ClusterNamesCSV=ClusterNamesCSV_prod
 fi
 
 echo "Deploying ecs-right-size-cluster..."
