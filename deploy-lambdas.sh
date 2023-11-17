cf0c3d385d1342fada71afac38357b2ee6b79086
briskt <3172830+briskt@users.noreply.github.com>
2023-11-17 14:49:12 -0700

    add a comment to identify the reason why the binary is named "bootstrap"

--- deploy-lambdas.sh
+++ deploy-lambdas.sh
@@ -6,7 +6,7 @@ set -e
 # Echo each command in the script
 set -x
 
-# build binary
+# When using the provided.al2 runtime, the binary must be named "bootstrap" and be in the root directory
 CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap
 
 STAGE="dev"
