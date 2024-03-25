846db4621ca81b34ce9fb472112af53ec5a567ee
briskt <3172830+briskt@users.noreply.github.com>
2024-03-25 14:25:57 -0600

    use buildvcs=false

--- deploy-lambdas.sh
+++ deploy-lambdas.sh
@@ -6,10 +6,8 @@ set -e
 # Echo each command in the script
 set -x
 
-git config --global --add safe.directory /src
-
 # When using the provided.al2 runtime, the binary must be named "bootstrap" and be in the root directory
-CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap
+CGO_ENABLED=0 go build -buildvcs=false -tags lambda.norpc -ldflags="-s -w" -o bootstrap
 
 STAGE="dev"
 export ClusterNamesCSV="${ClusterNamesCSV_dev}"
